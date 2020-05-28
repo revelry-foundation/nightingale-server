b09fd284137046f0ce011c1d1e4e2d5b0ee2e420
Curtis Cummings <curtis@parallaxvisions.com>
2020-05-28 12:50:28 -0500

    add positive_location schema

---
+++ lib/nightingale/positive_location.ex
@@ -0,0 +1,59 @@
+defmodule Nightingale.PositiveLocation do
+  use Ecto.Schema
+
+  import Ecto.{Query, Changeset}, warn: false
+
+  schema "positive_locations" do
+    field :json_blob, :map
+    field :location, Geo.PostGIS.Geometry
+    field :app_version, :string
+
+    timestamps()
+  end
+
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(
+      params,
+      [
+        :app_version,
+        :json_blob,
+        :version,
+        :document_status,
+      ]
+    )
+    |> unwrap_location()
+    |> validate_required([:customer_id, :contract, :location])
+  end
+
+  defp unwrap_location(changeset) do
+    if get_field(changeset, :location) == nil do
+      json = get_field(changeset, :json_blob)
+      coords = Map.get(json, :coordinates)
+
+      put_change(changeset, :location, generate_geo_point(coords))
+    end
+  end
+
+  defp generate_geo_point(nil), do: nil
+  defp generate_geo_point(%{lng: long, lat: lat}) do
+    %Geo.Point{coordinates: {long, lat}, srid: nil}
+  end
+
+end
+
+defimpl Jason.Encoder, for: Nightingale.PositiveLocation do
+  def encode(%{name: name, location: location}, opts) do
+    %{lat: lat, lng: lng} = decode_location(location)
+
+    Jason.Encode.map(%{name: name, lat: lat, lng: lng}, opts)
+  end
+
+  defp decode_location(%{coordinates: {lng, lat}}) do
+    %{lat: lat, lng: lng}
+  end
+
+  defp decode_location(%{}) do
+    %{lat: nil, lng: nil}
+  end
+end
---
+++ priv/repo/migrations/20200528161720_create_positive_locations_table.exs
@@ -0,0 +1,17 @@
+defmodule Nightingale.Repo.Migrations.CreatePositiveLocationsTable do
+  use Ecto.Migration
+
+  def up do
+    create table(:positive_locations) do
+      add(:location, :geometry)
+      add(:json_blob, :map)
+      add(:app_version, :string)
+
+      timestamps()
+    end
+  end
+
+  def down do
+    drop table(:positive_locations)
+  end
+end
